ac3fde200f4c928c6cf5351799cd382da7e72954
aadarshsingh109 <qp98500@gmail.com>
2025-12-29 14:55:14 +0530

    Fix import path for ColorContext component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar.jsx'
 import Notes from './components/Notes.jsx'
-import { ColorProvider } from './context/colorContext.jsx'
+import { ColorProvider } from './context/ColorContext.jsx'
 import './App.css'
 
 function App() {
